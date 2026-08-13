1d44b098549904490bb51e18a9ce5247474aa82f
HiteshSurya <hiteshsurya018@gmail.com>
2026-08-13 12:15:18 +0530

    Postpone website launch to 12 PM

--- lib/launch.ts
+++ lib/launch.ts
@@ -18,7 +18,7 @@ type LaunchStateOptions = {
  * 11:00 AM
  * India Standard Time (UTC+05:30)
  */
-const DEFAULT_LAUNCH_AT = "2026-08-13T12:15:00+05:30";
+const DEFAULT_LAUNCH_AT = "2026-08-13T12:25:00+05:30";
 
 function currentMs(now: number | Date | undefined) {
   if (now instanceof Date) {
